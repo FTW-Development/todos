71eec0a77ebd68c03d806866d72e51dabf2816cc
Michael Rudow <mrudow@ftwdev.com>
2013-07-10 09:36:45 -0400

    saving a bunch of small stuff which was from a while ago

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,6 +1,8 @@
 $(document).ajaxSuccess(function(event, xhr, settings) {
-  $('#tasks li:first-child').before('<li id="task'+xhr.responseJSON.id+'"><input id="complete" type="checkbox" value="completed" name="complete" /><span class="content">'+xhr.responseJSON.content+'</span><select id="user_id"><option value="">Choose One</option><option value="0">Hunter</option><option value="1">Andy</option><option vlaue="2">Michael</option>'+xhr.responseJSON.user_id+'</select><a rel="nofollow" data-method="delete" data-confirm="Are you sure?" href="/tasks/'+xhr.responseJSON.id+'">Destroy</a></td></tr>');
-  $('#content-field').val('');
+  $('#tasks li:first-child').before('<li id="task'+xhr.responseJSON.id+'"><input id="complete" type="checkbox" value="completed" name="complete" /><span class="content">'+xhr.responseJSON.content+'</span><select id="user_id"><option value="0">Hunter</option><option value="1">Andy</option><option vlaue="2">Michael</option>'+xhr.responseJSON.user_id+'</select><a rel="nofollow" data-method="delete" data-confirm="Are you sure?" href="/tasks/'+xhr.responseJSON.id+'">Destroy</a></td></tr>');
+  $('#content-field').val();
+  $('#user_id').val();
+  $('#edit-content').hide();
 });
 
 $(document).on( "sortupdate", function( event, ui ) {
@@ -11,7 +13,17 @@ $(document).on( "sortupdate", function( event, ui ) {
     success: function(result) {
     }
 });
-    
+
+$'(#edit-content').on("click", function() {
+  $('#edit-content').show();
+  $('.content').hide(); 
+});
+$(document).on('click', ':not(#edit-content)', function(e) { 
+  if(e.target.id !== 'edit-content') {
+    $('#edit-content').hide();
+    $('.content').show();
+  }    
+});
 } );
 
 $(function() {
@@ -19,5 +31,11 @@ $(function() {
     {
       cursor: 'crosshair',
     });
-  //$( "#tasks" ).disableSelection();
+  $( "#tasks" ).disableSelection();
 });
+$(document).ready(function() {
+  $('.new_task').on('mouseleave', function(
+      $(this).closest('.new_task').val(function(index,currentvalue));
+  );
+});
+
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ActiveRecord::Base
-  attr_accessible :completed, :content, :user_id, :complete
+  attr_accessible :completed, :content, :user_id, :complete, :position
   acts_as_list
   USER_ID= {0=>"Hunter", 1=>"Andy", 2=>"Michael"}
+  validates :user_id, :numericality=> { message: "must be selected from the drop-down list"}
 end
